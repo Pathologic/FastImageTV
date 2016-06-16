30a0e3f685c8362de46fda7a87ec99453899206b
Pathologic <maxx@np.by>
2016-06-17 01:25:36 +0300

    params description

--- assets/tvs/FastImage/config/default.php
+++ assets/tvs/FastImage/config/default.php
@@ -1,16 +1,33 @@
 <?php
-
+/**
+ * Плейсхолдеры в имени папки и файла:
+ * [+id+] - id документа
+ * [+parent+] - родитель документа
+ * [+uid+] - id менеджера
+ * [+rand+] - случайное значение
+ * [+time+] - unix-время
+ * [+year+] - год
+ * [+month+] - месяц
+ * [+day+] - день
+ * [+hour+] - час
+ * [+minute+] - минута
+ * [+second+] - секунда
+ * 
+ */
 $config = array();
 $config = array(
     "folder" => "assets/user_images/[+id+]/",
     "filename" => "[+month+]-[+day+]-[+filename+]",
     "allowed" => array('png','jpg','jpeg','gif'),
+    //настройки для преобразования изображения при загрузке
     "imageTransform" => array(
         "maxWidth" => 1920,
         "maxHeight" => 1080,
         "quality" => 0.93,
     ),
+    //преобразовывать на клиенте если true, иначе на сервере
     "clientResize" => true,
+    //превью в админке
     "thumbnail" => array(
         "folder" => ".thumbs",
         "options" => "w=200&h=200&far=C&bg=FFFFFF"
